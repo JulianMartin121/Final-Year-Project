65262d35831698b4df8f337b2dd4ea1cffbc63c0
JulianMartin121 <julianmartinwork21@gmail.com>
2024-02-01 10:50:21 +0000

    Module Pages

--- homepage/urls.py
+++ homepage/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('module1/', views.module1, name='module1'),
+    path('module2/', views.module2, name='module2'),
+    path('module3/', views.module3, name='module3'),
     path('', views.home, name='home'),
 ]
 
--- homepage/views.py
+++ homepage/views.py
@@ -5,4 +5,18 @@ def home(request):
     context = {}
     return render(request, 'homepage/home.html', context)
 
+def module1(request):
+    context = {}
+    return render(request, 'homepage/module1.html', context)
+
+def module2(request):
+    context = {}
+    return render(request, 'homepage/module2.html', context)
+
+def module3(request):
+    context = {}
+    return render(request, 'homepage/module3.html', context)
+
+
+
 
