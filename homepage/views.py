d0c8ffecbbac8f5b0c5975c750a5c557c440d132
JulianMartin121 <julianmartinwork21@gmail.com>
2024-03-18 11:01:21 +0000

    Node.JS changes

--- homepage/views.py
+++ homepage/views.py
@@ -8,19 +8,6 @@ def home(request):
     context = {}
     return render(request, 'homepage/home.html', context)
 
-# def role_required(request):
-#     # Decorator to check if the user is within the given role
-#     def decorator(view_func):
-#         @warps(view_func):
-#         def wrapper(request, *args, **kwargs):
-#             if request.user.user_type in roles:
-#                 return view_func(request, *args, **kwargs)
-#             else:
-#                 return HttpResponseForbidden()
-#         return wrapper
-#     return decorator
-
-
 @login_required
 def module1(request):
     context = {}
